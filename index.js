c1cf7d4ece519383245ee7fd7e98f6c75ae4c6d8
AtharvKadam21 <atharvkadam82@gmail.com>
2023-10-24 00:46:47 +0530

    removed work list and connected to mongodb

--- index.js
+++ index.js
@@ -1,32 +1,62 @@
 import express from "express";
 import bodyparser from "body-parser";
+import mongoose from "mongoose";
 
 const app = express();
 const port = 3000;
 
-var entryCheck = 0;
-
 app.use(express.static("public"));
 app.use(bodyparser.urlencoded({extended: true}));
 
+mongoose.connect('mongodb://127.0.0.1:27017/plannerDB')
+  .then(() => console.log('Connected!'));
+
+const Schema = mongoose.Schema;
+
+const itemsSchema = new Schema({
+    name: String
+});
+
+const Item = mongoose.model("Item", itemsSchema);
+
+// const item1 = new Item({
+//     name: "First Item"
+// });
+
+// const item2 = new Item({
+//     name: "Second Item"
+// });
+
+// const item3 = new Item({
+//     name: "Third Item"
+// });
+
+// const defaultItems = [item1, item2, item3];
+
+// Item.insertMany(defaultItems);
+
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    let foundItems = [];
+    Item.find({})
+        .then(foundItems =>{
+            res.render("index.ejs", {list1: foundItems});
+        })
+        .catch(err => {
+            console.log(err);
+    });
 });
 
 app.post("/submit", (req, res) => {
-    entryCheck = 1;
-    let taskEntry = req.body["task"];
-    if (req.body["dailyTask"]==="dailyTask"){
-        dailyList.push({name: taskEntry, completed: false});
-    }if(req.body["workTask"]==="workTask"){
-        workList.push({name: taskEntry, completed: false});
-    }    
-    res.render("index.ejs", {list1: dailyList, list2: workList, flag: entryCheck});
+    let foundItems = [];
+    const itemName = req.body["task"];
+    const item = new Item({
+        name: itemName
+    });
+    item.save();
+
+    res.redirect("/");
 });
 
 app.listen(port, () => {
     console.log(`Port ${port} active`);
 });
-
-var dailyList = [];
-var workList = [];
